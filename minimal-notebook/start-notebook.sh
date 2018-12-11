d580f8e4d2c7783786d4b633389d83d192a9eeab
Anand Ramanagoudar <anand.prodevans@gmail.com>
2018-12-11 15:12:02 +0530

    startup

--- minimal-notebook/start-notebook.sh
+++ minimal-notebook/start-notebook.sh
@@ -34,3 +34,4 @@ fi
 JUPYTER_PROGRAM_ARGS="$JUPYTER_PROGRAM_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
 
 exec /opt/app-root/bin/start.sh jupyter notebook $JUPYTER_PROGRAM_ARGS "$@"
+exec /opt/app-root/bin/startup.sh 
